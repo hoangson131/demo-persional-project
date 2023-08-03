24acc09487bfcf0dd76d4540b477cf4ed5548eeb
Vo Hoang Son <Vohoangson011218@gmail.com>
2023-08-03 22:21:29 +0700

    Update

---
+++ src/Layouts/LayoutFootandNav/index.js
@@ -0,0 +1,22 @@
+import classNames from 'classnames/bind';
+
+
+import styles from './LayoutFootandNav.module.scss'
+import { OnlyNavbar } from '../components';
+import Footer from '../components/Footer';
+
+const cx = classNames.bind(styles)
+
+function LayoutFootandNav({children }) {
+    return ( 
+        <div className={cx('wrapper')}>
+            <OnlyNavbar/>
+            <div className={cx('content')}>
+                {children}
+            </div>
+            <Footer/>
+        </div>
+     );
+}
+
+export default LayoutFootandNav ;
